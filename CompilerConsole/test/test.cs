d18f75a3631dc1677815db16d9a6eb6140cdb755
Christopher Lewis <christophvlewis@gmail.com>
2017-09-26 05:53:26 -0400

    Added Xunit test References and a simple test.

---
+++ CompilerConsole/test/test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CompilerConsole.test
+{
+	public class test
+	{
+		[Fact]
+		public static void testtest()
+		{
+			Assert.True(true);
+		}
+	}
+}
