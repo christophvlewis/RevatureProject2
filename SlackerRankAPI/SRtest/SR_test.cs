97221026b8157a518e11e4bb972c246765a209de
Christopher Lewis <christophvlewis@gmail.com>
2017-09-26 06:33:14 -0400

    Added Xunit Stuff to other projects

---
+++ SlackerRankAPI/SRtest/SR_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SlackerRankAPI.SRtest
+{
+	public class SR_test
+	{
+		[Fact]
+		public static void SR_dummy_test()
+		{
+			Assert.True(true);
+		}
+	}
+}
