3d07ef9342f8bf3f1ce8a6931ce0625becb25738
Christopher Lewis <christophvlewis@gmail.com>
2017-09-27 09:08:54 -0400

    More Diagnostic

--- SlackerRank_preAlpha/sample_code/sample_stuff.cs
+++ SlackerRank_preAlpha/sample_code/sample_stuff.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace SlackerRank_preAlpha.sample_code
 {
