4cd245844240ce9dda54374b902b1be5278cb973
NickalasLight <nickalas.93@gmail.com>
2017-10-04 20:09:28 -0400

    really big changes

---
+++ SlackerRank_preAlpha/Controllers/TestingJSController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlackerRank_preAlpha.Controllers
+{
+    public class TestingJSController : Controller
+    {
+
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SlackerRank_preAlpha/wwwroot/js/app-problemlist.js
@@ -0,0 +1,18 @@
+﻿//page for display of problem list
+(function () {
+    "use strict";
+    //creating module ('[]' signifies this)
+    angular.module("app-problemlist", ["ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/testingjs", {
+
+                controller: "problemlistController",
+                controllerAs: "vm",
+                templateUrl:"/Compiler"
+            });
+
+            $routeProvider.otherwise({redirectTo: "/"});
+
+        });
+})();
